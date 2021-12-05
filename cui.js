2ad0b3ee3fba60d94b34339b3c84c1e40992325e
kailun <54568547+KAILUENG@users.noreply.github.com>
2021-12-05 15:26:30 +0800

    1.0

--- cui.js
+++ cui.js
@@ -1,45 +1,55 @@
 
-/* 实现手机端tab展开 */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropmenu')) {
-
-    var dropdowns = document.getElementsByClassName("dropmenu-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
 /* 实现展开菜单 */
 function cuidroplist() {
   document.getElementById("cuidroplist").classList.toggle("listshow");
 }
 
 /* 实现snackbar */
-
 function cuisnackbarshow() {
-  var x = document.getElementById("cuisnackbar")
+  var x = document.getElementById("cuisnackbar");
   x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 
 
-function jumpdowload() {
+window.onload = () => {
+  const menu = document.querySelector('.menu')
+  const menuHeight = menu.offsetHeight - parseInt(getComputedStyle(menu)['paddingTop']) - parseInt(getComputedStyle(menu)['paddingBottom'])
+  menu.style.height = '0'
 
-  window.open("https://www.aliyundrive.com/s/AVDCYbJxc73");
+  openMenu = e => {
+      e.preventDefault()
 
+      menu.style.left = `${e.clientX}px`
+      menu.style.top = `${e.clientY + 5}px`
+      menu.style.height = `${menuHeight}px`
+      menu.classList.add('is-active')
+
+      return false
+  }
+
+  colseMenu = () => {
+      menu.style.height = '0'
+      menu.classList.remove('is-active')
+  }
+
+  window.onclick = () => colseMenu()
 }
 
-function jumpcui() {
-  window.open("https://github.com/OUR-Studio-cui-team/connect-UI")
+let shareData = {
+  title: 'Huton design',
+  text: 'have a nice weekend',
+  url: 'https://connect.ourstudio.top/',
+}
+
+const btn = document.querySelector('#sharingcui');
+const resultPara = document.querySelector('.result');
+
+btn.addEventListener('click', () => {navigator.share(shareData)});
+
+function jump(){
+    window.location.href="https://www.aliyundrive.com/s/qfoSdVZaivc";
+}
+function jump2(){
+    window.location.href="https://github.com/connect-ui/ui";
 }
